2a7b9843d09a3269353c07981058ce437dee9ac7
micaelalv02 <alvmicaela02@gmail.com>
2022-09-27 12:11:27 -0300

    avances 27-09

--- format/admin/updateimage.php
+++ format/admin/updateimage.php
@@ -1,1 +1,7 @@
-.
+9<?php
+include("assets/includes/header.inc.php");
+include("assets/includes/nav.inc.php");
+$classes = isset($_GET["class"]) ? $_GET["class"] : 'contents';
+$action = isset($_GET["action"]) ? $_GET["action"] : 'list';
+include('assets/includes/' . $classes . "/" . $action . ".inc.php");
+include("assets/includes/footer.inc.php");
